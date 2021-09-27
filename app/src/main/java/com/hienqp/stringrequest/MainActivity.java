cfe36f91cd81cbd75ebd44b40997195004fcb6af
hienqp <hienphan5290@gmail.com>
2021-09-27 20:43:54 +0700

    su dung Volley API Networking, request 1 StringRequest tu URL chi dinh

---
+++ app/src/main/java/com/hienqp/stringrequest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.hienqp.stringrequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // khởi tạo RequestQueue
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        String url = "https://online.khoapham.vn/";
+
+        // khởi tạo StringRequest
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("LOI", "Lỗi: \n" + error);
+                    }
+                });
+
+        // ở RequestQueue add StringRequest
+        requestQueue.add(stringRequest);
+    }
+}
